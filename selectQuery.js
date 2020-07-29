ce57d605fab8e9421b352a54669cfe41fd8550fd
shivanichauhan18 <shivanic18@navgurukul.org>
2020-07-29 09:32:21 +0530

    select data from mongodb

---
+++ selectQuery.js
@@ -0,0 +1,28 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+const selectOne = ()=>{
+MongoClient.connect(url,function(err,db){
+    if (err) throw err;
+    var dbo = db.db("mydb");                                                    
+    dbo.collection("customers").findOne({}, function(err, result) {
+        if (err) throw err;
+        console.log(result)
+    })
+})
+}
+
+const selectAll = ()=>{
+    MongoClient.connect(url,function(err,db){
+        if (err) throw err;
+        var dbo = db.db("mydb");  
+        dbo.collection("customers").find({},{ projection: { _id: 0, name: 1,address:1 } }).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+          });
+})
+}
+selectAll()
+
+
