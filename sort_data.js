3dc56df8a7f2c9c031ef617ab3b293265b195a59
shivanichauhan18 <shivanic18@navgurukul.org>
2020-07-29 09:51:47 +0530

    select data USING opration

---
+++ sort_data.js
@@ -0,0 +1,29 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+
+// ASSENDING ORDER DATA 
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var mysort = { name: 1 };
+  dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    // console.log(result);
+    db.close();
+  });
+});
+
+// DISENDING ORDER DATA 
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var mysort = { name: -1 };
+    dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+
